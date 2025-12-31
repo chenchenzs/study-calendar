580184b02d6591024247018aab35ffd020d14ee3
chenchen <929215951@qq.com>
2025-12-31 17:14:21 +0800

    fix:fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import dayjs from 'dayjs';
+import { Fragment, useState } from 'react'
+import { Calendar } from 'antd'
+import type { CalendarProps } from 'antd'
+import type { Dayjs } from 'dayjs'
+import 'dayjs/locale/zh-cn';
+dayjs.locale('zh-cn');
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const getMonthData = (value: Dayjs) => {
+    if (value.month() === 8) {
+      return 1394;
+    }
+  };
+
+  const monthCellRender = (value: Dayjs) => {
+    const num = getMonthData(value);
+    return num ? (
+      <div className="notes-month">
+        <section>{num}</section>
+        <span>Backlog number</span>
+      </div>
+    ) : null;
+  };
+
+  const cellRender: CalendarProps<Dayjs>['cellRender'] = (current, info) => {
+    // if (info.type === 'date') {
+    //   return dateCellRender(current);
+    // }
+    if (info.type === 'month') {
+      return monthCellRender(current);
+    }
+    return info.originNode;
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>HELLO WORLD</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Fragment>
+      <Calendar mode='month' cellRender={cellRender} />
+    </Fragment>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
+import { ConfigProvider } from 'antd';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
+import locale from 'antd/locale/zh_CN';
+import dayjs from 'dayjs';
+
+import 'dayjs/locale/zh-cn';
+
+dayjs.locale('zh-cn');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ConfigProvider locale={locale}>
     <App />
+    </ConfigProvider>
   </StrictMode>,
 )
