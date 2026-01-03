bf5647ccd8e6013180e39f0972b4bcd4d196cd78
chenchen <929215951@qq.com>
2026-01-03 13:11:34 +0800

    fix: add study content

--- src/utils/initData.ts
+++ src/utils/initData.ts
@@ -2,31 +2,194 @@
 
 const initData : any[] = [
     {
-        "id": "ab83bb7c-26dd-4faf-840b-2e98e33cb796",
+        "id": "28eea67f-0b37-443b-be1f-bcbe86d826bd",
         "year": 2026,
         "month": 1,
-        "day": 2,
+        "day": 8,
         "data": [
             {
                 "significance": "proficiency",
-                "content": "12345",
-                "id": "afa8261b-57bb-43bf-a508-5da7cdf92b45"
+                "content": "3.1 语法",
+                "id": "69f76f48-2da3-4709-a4ce-c94aa9927ff3"
+            },
+            {
+                "significance": "proficiency",
+                "content": "3.3 变量",
+                "id": "a05aa488-0ed8-4181-a9eb-a402262743fd"
+            },
+            {
+                "significance": "proficiency",
+                "content": "3.4 数据类型",
+                "id": "64c01c8e-0b4d-4ed0-b8a7-56df7880428b"
+            },
+            {
+                "significance": "proficiency",
+                "content": "(ES6) let 和 const 命令",
+                "id": "2fd5f0d4-30f5-42db-b5dc-71903e22fdbd"
+            },
+            {
+                "significance": "proficiency",
+                "content": "(ES6) 变量的解构赋值",
+                "id": "d36cc479-2377-40f1-a9ce-eaaef4c0f021"
             }
         ]
     },
     {
-        "id": "e179e1fb-1664-48ce-a86e-7f9253d2e54a",
+        "id": "17874598-546c-47df-9d7f-3fab6e8de1a5",
         "year": 2026,
         "month": 1,
-        "day": 3,
+        "day": 9,
         "data": [
             {
-                "significance": "know",
-                "content": "23456",
-                "id": "04a7e159-aca7-4409-b40c-557cc0e797da"
+                "significance": "proficiency",
+                "content": "创建一个React项目",
+                "id": "0181f0d8-36b2-4035-ac5c-2573be46b90f"
+            },
+            {
+                "significance": "proficiency",
+                "content": "使用 useState写一个计数器",
+                "id": "d110bcab-173c-4452-97eb-c20108307fec"
+            },
+            {
+                "significance": "proficiency",
+                "content": "把计数器封装成组件并引入",
+                "id": "120e5318-b872-4a0c-aa7c-83c657a2d961"
             }
         ]
     },
+    {
+        "id": "d52a320b-1958-4fba-9d4d-a5204706c528",
+        "year": 2026,
+        "month": 1,
+        "day": 10,
+        "data": [
+            {
+                "significance": "understand",
+                "content": "(Antd) Card",
+                "id": "bbdd36ea-21ea-4cd4-92cb-35159d7975bd"
+            },
+            {
+                "significance": "understand",
+                "content": "(Antd) Table",
+                "id": "ef0e7f19-82ed-41ed-b159-d7bbf9a0bd61"
+            },
+            {
+                "significance": "understand",
+                "content": "(Antd) Modal",
+                "id": "9f947001-3c2d-4d1d-8a1b-d7f0d690f74e"
+            }
+        ]
+    },
+    {
+        "id": "2df68459-ade5-4b1c-953e-3342671df1d1",
+        "year": 2026,
+        "month": 1,
+        "day": 15,
+        "data": [
+            {
+                "significance": "proficiency",
+                "content": "4.1 原始值与引用值",
+                "id": "65a76392-54dc-46fc-a04c-08e11953d6d6"
+            },
+            {
+                "significance": "proficiency",
+                "content": "4.2 执行上下文与作用域",
+                "id": "e1fac293-2832-4236-87eb-4427d5f24e99"
+            },
+            {
+                "significance": "proficiency",
+                "content": "5.3 原始值包装类型",
+                "id": "e0479c4d-4aae-468f-96fc-2ea6dfdf60ce"
+            },
+            {
+                "significance": "understand",
+                "content": "(ES6) 字符串的扩展",
+                "id": "222ccee7-6e5d-49b5-9b7b-9a3ed5e8837c"
+            },
+            {
+                "significance": "know",
+                "content": "(ES6) 字符串的新增方法",
+                "id": "98a7f5da-0afb-42c0-8052-66b7053aa2d2"
+            },
+            {
+                "significance": "know",
+                "content": "(ES6) 数值的扩展",
+                "id": "90b7add7-e145-43ef-b1e8-5b1e8594b82e"
+            }
+        ]
+    },
+    {
+        "id": "8b2ab08c-f8d2-4205-9b47-6c7bd9086519",
+        "year": 2026,
+        "month": 1,
+        "day": 16,
+        "data": [
+            {
+                "significance": "proficiency",
+                "content": "生成两个计数器，并实现独立计数",
+                "id": "d6a227fb-fa96-425b-9c99-b69a50c91519"
+            }
+        ]
+    },
+    {
+        "id": "bb985e72-6ea9-4f81-aa9a-54c7c6e20421",
+        "year": 2026,
+        "month": 1,
+        "day": 17,
+        "data": [
+            {
+                "significance": "proficiency",
+                "content": "(Antd) Form",
+                "id": "64434386-2221-428c-b822-8f1c798a6005"
+            }
+        ]
+    },
+    {
+        "id": "d9b2f732-60aa-420e-bdf5-6b35679cffc4",
+        "year": 2026,
+        "month": 1,
+        "day": 23,
+        "data": [
+            {
+                "significance": "proficiency",
+                "content": "6.1 Object",
+                "id": "594c4ac3-b3ef-4d9e-8a28-96d928d5e5ce"
+            },
+            {
+                "significance": "proficiency",
+                "content": "6.2 Array",
+                "id": "87b9a89f-5d12-4080-bb66-9ce63f262e43"
+            },
+            {
+                "significance": "proficiency",
+                "content": "(ES6) 数组的扩展",
+                "id": "55d0288f-1528-4e7b-9f36-d2e6a62679cc"
+            },
+            {
+                "significance": "proficiency",
+                "content": "(ES6) 对象的扩展",
+                "id": "b8597b76-46bf-49e0-acc3-4343386c60ea"
+            }
+        ]
+    },
+    {
+        "id": "cb4336be-8e42-411e-beb4-60fa8638d9d4",
+        "year": 2026,
+        "month": 1,
+        "day": 30,
+        "data": [
+            {
+                "significance": "proficiency",
+                "content": "第 10 章 函 数",
+                "id": "59edb92b-70d0-48eb-a479-0ae9b4a7f9e5"
+            },
+            {
+                "significance": "proficiency",
+                "content": "(ES6) 函数的扩展",
+                "id": "4bea326f-8f24-487a-a4c2-774cfd70c91d"
+            }
+        ]
+    }
 ];
 
 export default initData;
